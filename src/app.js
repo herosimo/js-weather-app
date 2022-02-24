4e5b8f2372e169cd17e5f48437621bfc9af9b14e
Herosimo Sribiko <herosimo06@gmail.com>
2022-02-24 21:30:44 +0700

    Add api response to frontend

--- src/app.js
+++ src/app.js
@@ -2,36 +2,136 @@ export class App {
   constructor(api) {
     this.api = api;
 
+    this.body = document.body;
+    this.searchIcon = document.querySelector(".left__top__searchIcon");
     this.searchForm = document.querySelector(".search");
     this.searchLoading = document.querySelector(".search__loading");
+    this.closeIcon = document.querySelector(".close");
+    this.modal = document.querySelector(".modal");
     this.cityList = document.querySelector(".city-list");
-    this.weatherDetails = document.querySelector(".weather-details__content");
-    this.weatherDetailsLoading = document.querySelector(".weather-details__loading");
+    // this.weatherDetails = document.querySelector(".weather-details__content");
+    // this.weatherDetailsLoading = document.querySelector(".weather-details__loading");
+
+    // Temperature details
+    this.leftScreenTemp = document.querySelector(".left__bottom__temp span");
+    this.leftScreenTitle = document.querySelector(".left__bottom__temp h2");
+    this.leftScreenSubtitle = document.querySelector(".left__bottom__temp p");
+    this.leftScreenFl = document.querySelector(".left__bottom__fl");
+    this.leftScreenDate = document.querySelector(".left__bottom__footer__date");
+    this.leftScreenLocation = document.querySelector(".left__bottom__footer__location");
+
+    // Right screen weather details
+    this.rightScreenAqi = document.querySelector(".right__details__aqi");
+    this.rightScreenHumidity = document.querySelector(".right__details__humidity");
+    this.rightScreenPressure = document.querySelector(".right__details__pressure");
+    this.rightScreenUvi = document.querySelector(".right__details__uvi");
+    this.rightScreenClouds = document.querySelector(".right__details__clouds");
+    this.rightScreenVisibility = document.querySelector(".right__details__visibility");
+    this.rightScreenWind = document.querySelector(".right__details__wind");
+    this.rightScreenNextHours = document.querySelector(".right__nextHours");
+    this.rightScreenNextDays = document.querySelector(".right__nextDays");
 
     this.toggleHideView(this.searchLoading, true);
     this.toggleHideView(this.cityList, true);
-    this.toggleHideView(this.weatherDetails, true);
-    this.toggleHideView(this.weatherDetailsLoading, true);
+    // this.toggleHideView(this.weatherDetails, true);
+    // this.toggleHideView(this.weatherDetailsLoading, true);
+    this.toggleHideView(this.modal, true);
 
     this.searchForm.addEventListener("submit", this.onSubmitForm);
+    this.searchIcon.addEventListener("click", () => this.toggleHideView(this.modal, false));
+    this.closeIcon.addEventListener("click", () => this.toggleHideView(this.modal, true));
   }
 
-  toggleHideView(element, show) {
-    element.classList.toggle("hide", show);
+  toggleHideView(element, hide) {
+    element.classList.toggle("hide", hide);
   }
 
   onClickCity = async (e) => {
-    this.toggleHideView(this.weatherDetailsLoading, false);
+    // this.toggleHideView(this.weatherDetailsLoading, false);
 
     const oneCallData = await this.api.callOneCall(e.target.dataset.lat, e.target.dataset.lon);
-    const AirPollutionData = await this.api.callAirPollution(
+    const airPollutionData = await this.api.callAirPollution(
       e.target.dataset.lat,
       e.target.dataset.lon
     );
-    console.log("a", oneCallData);
-    console.log(AirPollutionData);
-    this.toggleHideView(this.weatherDetailsLoading, true);
-    this.toggleHideView(this.weatherDetails, false);
+    // console.log("a", oneCallData);
+    console.log(airPollutionData);
+
+    // Format time to always have 2 digits
+    const formatTime = (time) => {
+      return ("0" + time).slice(-2);
+    };
+
+    // Change left screen value
+    let date = new Date(oneCallData.current.dt * 1000);
+
+    this.leftScreenTemp.innerText = `${oneCallData.current.temp}°C`;
+    this.leftScreenTitle.innerText = oneCallData.current.weather[0].main;
+    this.leftScreenSubtitle.innerText = oneCallData.current.weather[0].description;
+    this.leftScreenFl.innerText = `Feels like ${oneCallData.current.feels_like}°C`;
+    this.leftScreenDate.innerText = `${formatTime(date.getHours())}:${formatTime(
+      date.getMinutes()
+    )} - ${date.toLocaleDateString("en-US", {
+      weekday: "long",
+    })}, ${date.getDate()} ${date.toLocaleDateString("en-US", {
+      month: "long",
+    })} ${date.getFullYear()}`;
+    this.leftScreenLocation.innerText = `${e.target.dataset.name}, ${e.target.dataset.state}, ${e.target.dataset.country}`;
+
+    // Change right screen value
+    let visibility = oneCallData.current.visibility / 1000;
+    let windSpeed = (oneCallData.current.wind_speed * (60 * 60)) / 1000;
+
+    let airPollutionDataVal = airPollutionData.list[0].main.aqi;
+    const airPollutionDataDetails = ["Good", "Fair", "Moderate", "Poor", "Very Poor"];
+    this.rightScreenAqi.innerText = `${airPollutionDataVal} - ${
+      airPollutionDataDetails[airPollutionDataVal - 1]
+    }`;
+    this.rightScreenHumidity.innerText = `${oneCallData.current.humidity}%`;
+    this.rightScreenPressure.innerText = `${oneCallData.current.pressure}hPa`;
+    this.rightScreenUvi.innerText = `${oneCallData.current.uvi}`;
+    this.rightScreenClouds.innerText = `${oneCallData.current.clouds}%`;
+    this.rightScreenVisibility.innerText = `${visibility.toFixed(1)}km`;
+    this.rightScreenWind.innerText = `${windSpeed.toFixed(1)}km/h`;
+
+    // Next hours
+    let nextHourELements = "";
+    let hourlyData = oneCallData.hourly.slice(0, 18);
+    hourlyData.forEach((data) => {
+      let date = new Date(data.dt * 1000);
+
+      nextHourELements += `
+      <li>
+        <span>${formatTime(date.getHours())}:${formatTime(date.getMinutes())}</span>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+        <span>${data.temp}°C</span>
+      </li>
+      `;
+    });
+
+    this.rightScreenNextHours.innerHTML = nextHourELements;
+
+    // Next days
+    let nextDayELements = "";
+    oneCallData.daily.forEach((data) => {
+      let date = new Date(data.dt * 1000);
+
+      nextDayELements += `
+      <li>
+        <span>${date.toLocaleDateString("en-US", {
+          weekday: "short",
+        })}, ${date.getDate()} ${date.toLocaleDateString("en-US", {
+        month: "short",
+      })} ${date.getFullYear()}</span>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+        <span>${data.temp.min}°C/${data.temp.max}°C</span>
+      </li>
+      `;
+    });
+
+    this.rightScreenNextDays.innerHTML = nextDayELements;
+    // this.toggleHideView(this.weatherDetailsLoading, true);
+    // this.toggleHideView(this.weatherDetails, false);
   };
 
   onSubmitForm = async (e) => {
@@ -45,7 +145,7 @@ export class App {
       let elements = "";
 
       locations.forEach((location) => {
-        elements += `<li data-lon="${location.lon}" data-lat="${location.lat}" class="city">${location.name}, ${location.state}, ${location.country}</li>`;
+        elements += `<li data-lon="${location.lon}" data-lat="${location.lat}" data-name="${location.name}" data-state="${location.state}" data-country="${location.country}" class="city">${location.name}, ${location.state}, ${location.country}</li>`;
       });
 
       this.cityList.firstElementChild.innerHTML = elements;
